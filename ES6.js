9f1f531929790113bf55d891f32384ab27b322e1
harriselliman <harris.elliman@hotmail.co.uk>
2019-03-30 18:14:03 +0000

    use destructuring assignment to assign variables from nested objects

--- ES6.js
+++ ES6.js
@@ -83,3 +83,22 @@ function getTempOfTmrw(avgTemperatures) {
 }
 
 console.log(getTempOfTmrw(AVG_TEMPERATURES)); // 79
+
+
+
+
+//Use Destructuring Assignment to Assign Variables from Nested Objects
+//Use destructuring assignment to obtain max of forecast.tomorrow and assign it to maxOfTomorrow
+
+const LOCAL_FORECAST = {
+  today: { min: 72, max: 83 },
+  tomorrow: { min: 73.3, max: 84.6 }
+};
+
+function getMaxOfTmrw(forecast) {
+  "use strict";
+  const { tomorrow : {max : maxOfTomorrow }} = forecast;
+  return maxOfTomorrow;
+}
+
+console.log(getMaxOfTmrw(LOCAL_FORECAST)); // 84.6
