365caa9cef976c7319aaef9cfff4c449c4eefb15
harriselliman <harris.elliman@hotmail.co.uk>
2019-03-30 17:48:07 +0000

    use destructuring assignment to assign variables from objects

--- ES6.js
+++ ES6.js
@@ -65,3 +65,21 @@ let arr2;
 })();
 
 console.log(arr2); //returns ['JAN', 'FEB', 'MAR', 'APR', 'MAY']
+
+
+
+//Use Destructuring Assignment to Assign Variables from Objects
+//Use destructuring to obtain the average temperature for tomorrow from the input object AVG_TEMPERATURES, and assign value with key tomorrow to tempOfTomorrow in line
+
+const AVG_TEMPERATURES = {
+  today: 77.5,
+  tomorrow: 79
+};
+
+function getTempOfTmrw(avgTemperatures) {
+  "use strict";
+  const { tomorrow : tempOfTomorrow } = avgTemperatures; 
+  return tempOfTomorrow;
+}
+
+console.log(getTempOfTmrw(AVG_TEMPERATURES)); // 79
