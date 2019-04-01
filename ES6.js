5ce99fd836e5c096bc53c9427b57309ba0a20012
harriselliman <harris.elliman@hotmail.co.uk>
2019-04-01 12:50:07 +0100

    create strings using template literals

--- ES6.js
+++ ES6.js
@@ -151,3 +151,32 @@ const half = (function() {
 })();
 console.log(stats); // should be object
 console.log(half(stats)); // 28.015
+
+
+
+//Create Strings using Template Literals
+//Use template literal syntax with backticks to display each entry of the result object's failure array. Each entry should be wrapped inside an li element with the class attribute text-warning, and listed within the resultDisplayArray
+
+const result = {
+  success: ["max-length", "no-amd", "prefer-arrow-functions"],
+  failure: ["no-var", "var-on-top", "linebreak"],
+  skipped: ["id-blacklist", "no-dup-keys"]
+};
+function makeList(arr) {
+  "use strict";
+
+  const resultDisplayArray = [
+    `<li class="text-warning">${arr[0]}</li>`,
+    `<li class="text-warning">${arr[1]}</li>`,
+    `<li class="text-warning">${arr[2]}</li>`
+    ];
+
+  return resultDisplayArray;
+}
+/**
+ * makeList(result.failure) should return:
+ * [ `<li class="text-warning">no-var</li>`,
+ *   `<li class="text-warning">var-on-top</li>`, 
+ *   `<li class="text-warning">linebreak</li>` ]
+ **/
+const resultDisplayArray = makeList(result.failure);
