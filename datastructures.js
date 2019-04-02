50baa0a83a36eb52ca820050387a162b367a7477
harriselliman <harris.elliman@hotmail.co.uk>
2019-04-02 23:40:13 +0100

    copy array items using slice()

--- datastructures.js
+++ datastructures.js
@@ -60,3 +60,14 @@ function htmlColorNames(arr) {
 } 
   
 console.log(htmlColorNames(['DarkGoldenRod', 'WhiteSmoke', 'LavenderBlush', 'PaleTurqoise', 'FireBrick']));
+
+
+
+//Copy Array Items Using slice()
+//Modify the function using slice() to extract information from the argument array and return a new array that contains the elements 'warm' and 'sunny'.
+
+function forecast(arr) {
+    return arr.slice(2, 4);
+}
+  
+console.log(forecast(['cold', 'rainy', 'warm', 'sunny', 'cool', 'thunderstorms']));
