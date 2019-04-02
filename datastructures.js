1c1b987361bd3a3ea73f98b245b9f37195fbe53e
Harris Woei Chen Elliman <harriselliman@Harriss-MacBook-Pro.local>
2019-04-02 20:53:49 +0100

    use bracket notation to access an arrays contents

--- datastructures.js
+++ datastructures.js
@@ -2,3 +2,13 @@
 //Complete the statement by assigning an array of at least 5 elements in length to the yourArray variable. Your array should contain at least one string, one number, and one boolean
 
 let yourArray = [1, true, 'three', 4, false, {five: 5, six: "six"},[7, "eight", 9]];
+
+
+
+
+//Access an Array's Contents Using Bracket Notation
+//set the 2nd position (index 1) of myArray to anything you want, besides "b"
+
+let myArray = ["a", "b", "c", "d"];
+myArray[1] = "better upgrade this b to an a";
+console.log(myArray);
