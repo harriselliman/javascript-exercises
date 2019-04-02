43dcc1137fd26bf1bc20a9ff96db4fa03f59d190
Harris Woei Chen Elliman <harriselliman@Harriss-MacBook-Pro.local>
2019-04-02 20:49:57 +0100

    use an array

---
+++ datastructures.js
@@ -0,0 +1,4 @@
+//Use an Array to Store a Collection of Data
+//Complete the statement by assigning an array of at least 5 elements in length to the yourArray variable. Your array should contain at least one string, one number, and one boolean
+
+let yourArray = [1, true, 'three', 4, false, {five: 5, six: "six"},[7, "eight", 9]];
