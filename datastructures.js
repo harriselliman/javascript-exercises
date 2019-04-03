42d8ca923ea12a82ba0740f9f7c599ee6b901f2f
harriselliman <harris.elliman@hotmail.co.uk>
2019-04-03 16:43:07 +0100

    add key value pairs to javascript objects

--- datastructures.js
+++ datastructures.js
@@ -166,6 +166,22 @@ let myNestedArray = [
     ]
   ];
   
-  console.log(myNestedArray[1][1][0]); // deep
-  console.log(myNestedArray[2][0][1][0]); // deeper
-  console.log(myNestedArray[3][0][0][2][0]);// deepest
+console.log(myNestedArray[1][1][0]); // deep
+console.log(myNestedArray[2][0][1][0]); // deeper
+console.log(myNestedArray[3][0][0][2][0]);// deepest
+
+
+
+//Add Key-Value Pairs to JavaScript Objects
+
+let foods = {
+    apples: 25,
+    oranges: 32,
+    plums: 28
+};
+  
+foods.bananas = 13;
+foods.grapes = 35;
+foods.strawberries = 27;
+  
+console.log(foods);
