c314689709f4cd7e2f3131ca84ed5d97d073fa59
harriselliman <harris.elliman@hotmail.co.uk>
2019-04-03 17:23:25 +0100

    generate array of object keys

--- datastructures.js
+++ datastructures.js
@@ -312,3 +312,33 @@ function countOnline(obj) {
 }
   
 console.log(countOnline(users)); //2
+
+
+
+//Generate an Array of All Object Keys with Object.keys()
+//write the getArrayOfUsers function so that it returns an array containing all the properties in the object it receives as an argument
+
+let users = {
+    Alan: {
+      age: 27,
+      online: false
+    },
+    Jeff: {
+      age: 32,
+      online: true
+    },
+    Sarah: {
+      age: 48,
+      online: false
+    },
+    Ryan: {
+      age: 19,
+      online: true
+    }
+};
+  
+  function getArrayOfUsers(obj) {
+    return Object.keys(obj);
+}
+  
+console.log(getArrayOfUsers(users));
