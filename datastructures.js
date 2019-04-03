876f9fab43dc648989ed989c84939e23bf88b8a3
harriselliman <harris.elliman@hotmail.co.uk>
2019-04-03 16:50:53 +0100

    remove object properties using delete

--- datastructures.js
+++ datastructures.js
@@ -223,3 +223,23 @@ function checkInventory(scannedItem) {
   
 console.log(checkInventory("apples"));
 console.log(checkInventory("grapes"));
+
+
+
+//Use the delete Keyword to Remove Object Properties
+//Use the delete keyword to remove the oranges, plums, and strawberries keys from the foods object
+
+let foods = {
+    apples: 25,
+    oranges: 32,
+    plums: 28,
+    bananas: 13,
+    grapes: 35,
+    strawberries: 27
+};
+  
+delete foods.oranges;
+delete foods.plums;
+delete foods['strawberries'];
+  
+console.log(foods);
