ade45978e7c7119986fa1a139d56866d0a18d8d1
harriselliman <harris.elliman@hotmail.co.uk>
2019-04-03 17:16:19 +0100

    iterate through object keys with for...in statement

--- datastructures.js
+++ datastructures.js
@@ -276,3 +276,39 @@ function isEveryoneHere(obj) {
 }
   
 console.log(isEveryoneHere(users));
+
+
+
+//Iterate Through the Keys of an Object with a for...in Statement
+//use a for...in statement within this function to loop through the users in the users object and return the number of users whose online property is set to true
+
+let users = {
+    Alan: {
+      age: 27,
+      online: false
+    },
+    Jeff: {
+      age: 32,
+      online: true
+    },
+    Sarah: {
+      age: 48,
+      online: false
+    },
+    Ryan: {
+      age: 19,
+      online: true
+    }
+};
+  
+function countOnline(obj) {
+    let count = 0;
+    for(let user in users){
+      if(users[user].online === true) {
+        count += 1;
+      }
+    }
+    return count;
+}
+  
+console.log(countOnline(users)); //2
